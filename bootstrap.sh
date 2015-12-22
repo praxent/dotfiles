29868699280e7f0e85b315fbab0334c85e4f1822
James <james@praxent.com>
2015-12-22 15:38:17 -0600

    add bootstrap .sh file
    Signed-off-by: James <james@praxent.com>

---
+++ bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+cd "$(dirname "${BASH_SOURCE}")"
+git pull origin master
+function doIt() {
+  rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "*.un~" \
+    --exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+  source ~/.bash_profile
+}
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+  doIt
+else
+  read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    doIt
+        chmod 644 $HOME/.ssh/config*
+  fi
+fi
+unset doIt
